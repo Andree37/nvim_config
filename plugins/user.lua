b257c48ae4741b0a63e2d8cdd89519d74c7f8fe0
Andre Ribeiro <andresmribeiro@gmail.com>
2023-05-23 13:31:41 +0100

    markdown previewer

--- plugins/user.lua
+++ plugins/user.lua
@@ -21,4 +21,51 @@ return {
       desc = "Open TODOs in Telescope",
     } },
   },
+  {
+    "whonore/Coqtail",
+    opts = {},
+  },
+  {
+    "tomtomjhj/coq-lsp.nvim",
+    opts = {},
+  },
+  {
+    -- https://github.com/toppair/peek.nvim/issues/28
+    "toppair/peek.nvim",
+    run = "deno task --quiet build:fast",
+    keys = {
+      {
+        "<leader>mp",
+        "<cmd>lua require('peek').open()<cr>",
+        desc = "Open Markdown Preview",
+      },
+      {
+        "<leader>mc",
+        "<cmd>lua require('peek').close()<cr>",
+        desc = "Close Markdown Preview",
+      },
+    },
+    opts = {
+      auto_load = true, -- whether to automatically load preview when
+      -- entering another markdown buffer
+      close_on_bdelete = true, -- close preview window on buffer delete
+
+      syntax = true, -- enable syntax highlighting, affects performance
+
+      theme = "dark", -- 'dark' or 'light'
+
+      update_on_change = true,
+
+      app = "webview", -- 'webview', 'browser', string or a table of strings
+      -- explained below
+
+      filetype = { "markdown" }, -- list of filetypes to recognize as markdown
+
+      -- relevant if update_on_change is true
+      throttle_at = 200000, -- start throttling when file exceeds this
+      -- amount of bytes in size
+      throttle_time = "auto", -- minimum amount of time in milliseconds
+      -- that has to pass before starting new render
+    },
+  },
 }
